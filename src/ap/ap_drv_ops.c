420c7576fb8b242352df7b636a94bbfe1aade8fc
Eliad Peller <eliad@wizery.com>
2011-06-23 19:23:06 +0300

    hostapd: add uapsd_queues and max_sp fields
    Add uapsd_queues and max_sp fields to sta_info struct,
    and pass them to the sta_add callback.
    
    These values are determined by the WME ie in the assoc request.

--- src/ap/ap_drv_ops.c
+++ src/ap/ap_drv_ops.c
@@ -336,6 +336,8 @@ int hostapd_sta_add(struct hostapd_data *hapd,
 	params.listen_interval = listen_interval;
 	params.ht_capabilities = ht_capab;
 	params.flags = hostapd_sta_flags_to_drv(flags);
+	params.uapsd_queues = uapsd_queues;
+	params.max_sp = max_sp;
 	return hapd->driver->sta_add(hapd->drv_priv, &params);
 }
 
