be9699b2636cb410011b5133624046a0f84272cc
Guy Eilam <guy@wizery.com>
2011-06-26 00:17:14 +0300

    cli/ctrl_iface - added blacklist and log_level commands
    Added a blacklist and log_level commands for Android

--- wpa_supplicant/wpa_cli.c
+++ wpa_supplicant/wpa_cli.c
@@ -1224,6 +1224,33 @@ static int wpa_cli_cmd_bssid(struct wpa_ctrl *ctrl, int argc, char *argv[])
 }
 
 
+#ifdef ANDROID
+static int wpa_cli_cmd_blacklist(struct wpa_ctrl *ctrl, int argc, char *argv[])
+{
+	char cmd[256], *pos, *end;
+	int i, ret;
+
+	end = cmd + sizeof(cmd);
+	pos = cmd;
+	ret = os_snprintf(pos, end - pos, "BLACKLIST");
+	if (ret < 0 || ret >= end - pos) {
+		printf("Too long BLACKLIST command.\n");
+		return -1;
+	}
+	pos += ret;
+	for (i = 0; i < argc; i++) {
+		ret = os_snprintf(pos, end - pos, " %s", argv[i]);
+		if (ret < 0 || ret >= end - pos) {
+			printf("Too long BLACKLIST command.\n");
+			return -1;
+		}
+		pos += ret;
+	}
+
+	return wpa_ctrl_command(ctrl, cmd);
+}
+
+
 static int wpa_cli_cmd_log_level(struct wpa_ctrl *ctrl, int argc, char *argv[])
 {
 	char cmd[256], *pos, *end;
@@ -1248,6 +1275,7 @@ static int wpa_cli_cmd_log_level(struct wpa_ctrl *ctrl, int argc, char *argv[])
 
 	return wpa_ctrl_command(ctrl, cmd);
 }
+#endif /* ANDROID */
 
 
 static int wpa_cli_cmd_list_networks(struct wpa_ctrl *ctrl, int argc,
@@ -2497,12 +2525,10 @@ static struct wpa_cli_cmd wpa_cli_commands[] = {
 	{ "bssid", wpa_cli_cmd_bssid,
 	  cli_cmd_flag_none,
 	  "<network id> <BSSID> = set preferred BSSID for an SSID" },
-<<<<<<< HEAD
 	{ "log_level", wpa_cli_cmd_log_level,
 	  cli_cmd_flag_none,
 	  "<level> [<timestamp>] = update the log level/timestamp\n"
 	  "log_level = display the current log level and log options" },
-=======
 #ifdef ANDROID
 	{ "blacklist", wpa_cli_cmd_blacklist,
 	  cli_cmd_flag_none,
@@ -2514,7 +2540,6 @@ static struct wpa_cli_cmd wpa_cli_commands[] = {
 	  "<level> [<timestamp>] = update the log level/timestamp of wpa_supplicant\n"
 	  "log_level = display the current log level and log options" },
 #endif /* ANDROID */
->>>>>>> cli/ctrl_iface - added blacklist and log_level commands
 	{ "list_networks", wpa_cli_cmd_list_networks,
 	  cli_cmd_flag_none,
 	  "= list configured networks" },
