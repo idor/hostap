7ce1a2c083d33b1dafe9a38eec5e2efebca09d80
Luciano Coelho <coelho@ti.com>
2011-09-30 16:20:02 +0300

    wpa_s: check if sched_scan is supported before using it
    Previously, we were trying to use scheduled scan without checking
    whether the driver supports it.  This is okay, but it is not efficient
    because we need to go all the way down to the kernel driver before we
    find out it is not supported.
    
    This patch adds a capability flag in wpa_supplicant that is used to
    check whether sched_scan is supported or not.
    
    Signed-off-by: Luciano Coelho <coelho@ti.com>

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -740,7 +740,10 @@ struct wpa_driver_capa {
 	int max_scan_ssids;
 	int max_sched_scan_ssids;
 	int sched_scan_supported;
+<<<<<<< HEAD
 	int max_match_sets;
+=======
+>>>>>>> wpa_s: check if sched_scan is supported before using it
 
 	/**
 	 * max_remain_on_chan - Maximum remain-on-channel duration in msec
@@ -2925,7 +2928,6 @@ enum wpa_event_type {
 	 */
 	EVENT_IBSS_PEER_LOST,
 
-<<<<<<< HEAD
 	/**
 	 * EVENT_DRIVER_GTK_REKEY - Device/driver did GTK rekey
 	 *
@@ -2940,9 +2942,6 @@ enum wpa_event_type {
 	 * EVENT_SCHED_SCAN_STOPPED - Scheduled scan was stopped
 	 */
 	EVENT_SCHED_SCAN_STOPPED
-=======
-	EVENT_SCHED_SCAN_STOPPED,
->>>>>>> wpa_s: add scheduled scan driver operations
 };
 
 
